1a58f4ac7d6d4e7631b27a93fa22865b667e2de2
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2024-10-08 06:04:46 -0700

    Fix Verible Homebrew formula:
    - enable using homebrew's bazel instead of an externally installed bazel:
      - removed `env :std`, which prevented homebrew's bazel from being found.
      - specify a minimum macOS SDK version to enable modern C++ features used by
        Verible.
      - bypass hombrew's compiler shims, which don't work with bazel
    - formula lint fixes.

--- Formula/verible.rb
+++ Formula/verible.rb
@@ -7,29 +7,32 @@ class Verible < Formula
       revision: "a939466243915e6151d4a3675c3e9689e94e8f8a"
   version "0.0-3665-ga9394662"
   license "Apache-2.0"
-  env :std
-  head do
-    url "https://github.com/chipsalliance/verible.git"
-  end
+  head "https://github.com/chipsalliance/verible.git", branch: "master"
 
   depends_on "bazel" => :build
+  depends_on macos: :catalina # C++ features
 
   def install
-    optflag = if Hardware::CPU.arm? && OS.mac?
-      "-mcpu=apple-m1"
-    elsif build.bottle?
-      "-march=#{Hardware.oldest_cpu}"
-    else
-      "-march=native"
+    bazel_args = []
+    bazel_args << "--jobs=#{ENV.make_jobs}"
+    bazel_args << "--compilation_mode=opt"
+    bazel_args << "--linkopt=-Wl,-rpath,#{rpath}"
+    bazel_args << "--verbose_failures"
+    bazel_args << "--sandbox_debug"
+    # Bazel defaults to a macOS SDK version of 10.11
+    # (see https://github.com/bazelbuild/bazel/blob/master/src/main/starlark/builtins_bzl/common/xcode/providers.bzl)
+    # The macOS SDK 10.11 does not support all needed C++ standard library
+    # features. Therefore, use the current platform's version, since Homebrew
+    # builds and bottles are specific to the OS version.
+    bazel_args << "--macos_sdk_version=#{MacOS.version}"
+
+    with_env(
+      # Bazel's environment and path resolution break Homebrew's shim scripts
+      CC:  "/usr/bin/cc",
+      CXX: "/usr/bin/c++",
+    ) do
+      system "bazel", "build", *bazel_args, "//verilog/tools/..."
     end
-    bazel_args = %W[
-      --jobs=#{ENV.make_jobs}
-      --compilation_mode=opt
-      --copt=#{optflag}
-      --linkopt=-Wl,-rpath,#{rpath}
-      --verbose_failures
-    ]
-    system "bazel", "build", *bazel_args, "//verilog/tools/..."
 
     bin.install %w[
       bazel-bin/verilog/tools/diff/verible-verilog-diff
